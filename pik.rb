173ad04bb4d953dbeda66b762bed00de57949094
Richard Steinbrueck <richard.steinbrueck@googlemail.com>
2026-06-15 15:02:24 +0200

    fix(pik): drop redundant version line and bump to 1.0.0

--- pik.rb
+++ pik.rb
@@ -1,9 +1,8 @@
 class Pik < Formula
   desc "Interactive Kubernetes pod picker"
   homepage "https://github.com/jacek-kurlit/pik"
-  url "https://github.com/jacek-kurlit/pik/releases/download/0.18.1/pik-0.18.1-aarch64-apple-darwin.tar.gz"
-  sha256 "b781568358aac9f57a3a2efe6014e8e3d8ad5931cfa2074152c1d32fdc903de7"
-  version "0.18.1"
+  url "https://github.com/jacek-kurlit/pik/releases/download/1.0.0/pik-1.0.0-aarch64-apple-darwin.tar.gz"
+  sha256 "4c7d38cc7810d2234fe309aea83520a85c9b5c880f503bcc8fc4023b61ce4486"
   license "MIT"
 
   def install
